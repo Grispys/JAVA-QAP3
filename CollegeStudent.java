cfa87b08dcad47d2172db0246a3de18f8c86eba7
Grispys <grispanimation@gmail.com>
2024-11-13 13:37:07 -0330

    created and complketed collegestudent class

--- CollegeStudent.java
+++ CollegeStudent.java
@@ -1,3 +1,25 @@
 public class CollegeStudent extends Student{
-    
+    protected String major;
+    protected int year;
+
+    public CollegeStudent(String majorCourse, int currentYear, String name, int age, String gender, String idNum, double gpa){
+        super(name, age, gender, idNum, gpa);
+        this.major = majorCourse;
+        this.year = currentYear;
+    }
+    public void setMajor(String majorCourse){
+        this.major = majorCourse;
+    }
+
+    public void setYear(int currentYear){
+        this.year = currentYear;
+    }
+
+    public String getMajor(){
+        return("Major: " + this.major);
+    }
+
+    public String getYear(){
+        return("Year: " + this.year);
+    }
 }
