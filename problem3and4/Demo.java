15de89b09b15b82aea87e8d1015640bcbb886280
Grispys <grispanimation@gmail.com>
2024-11-17 22:03:49 -0330

    properly implementing Scaling and made the shapes array get scaled and printed before and after

--- problem3and4/Demo.java
+++ problem3and4/Demo.java
@@ -3,8 +3,10 @@ package problem3and4;
 
 public class Demo{
 
-    public static void Scaling(Scalable[] scalingFactor){
-
+    public static void Scaling(Scalable[] Scalable, double scalingFactor){
+        for (Scalable scalable : Scalable) {
+            scalable.scale(scalingFactor);
+        }
     }
 
 
@@ -15,7 +17,15 @@ public class Demo{
         shapes[1] = new Triangle(5, 6, 7, "Triangle");
         shapes[2] = new Ellipse(10, 13, "Ellipse");
         shapes[3] = new EquilateralTriangle(5, "ETriangle");
-
+        System.out.println();
+        System.out.println("BEFORE SCALING: ");
+        for (Shape shape : shapes) {
+            System.out.println(shape.toString());
+        }
+        System.out.println();
+        Scaling(shapes, 1.3);
+        System.out.println();
+        System.out.println("AFTER SCALING: ");
         for (Shape shape : shapes) {
             System.out.println(shape.toString());
         }
